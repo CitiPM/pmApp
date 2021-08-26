60a5b13f4f6574fc5136c1b97888d5fa0d4d3e39
David <gomezd6@mcmaster.ca>
2021-08-26 16:15:05 -0400

    Fixed indice name display

--- src/app/market-movers/market-movers.component.ts
+++ src/app/market-movers/market-movers.component.ts
@@ -9,27 +9,20 @@ import { MrktService } from 'src/services/mrkt.service';
 })
 export class MarketMoversComponent implements OnInit {
 
-  accountNames: string[] = [];
-
-  paramGL : string = "gainers";
-
   indicesNum: number[] = [];
   indicesNames: string[] = ["S&P 500", "Dow Jones Industrial Average", "Nasdaq Composite", "Russell 2000 Index"];
 
   constructor(private mrktService: MrktService) { }
 
   ngOnInit(): void {
+    this.mrktindexCall();
   }
   
   mrktindexCall(){
-    this.mrktService.getIndicesData().subscribe((data:any)=>{this.indicesNum = data})
+    this.mrktService.getIndicesData().subscribe((data:any)=>
+    {
+      this.indicesNum = data;
+      console.log(data);
+    })
   }
-
-  // makeServiceCall(){
-  //   console.log("clicked")
-  //   this.mrktService.getGLData(this.paramGL)
-  //     .subscribe( (data:any)=>{
-  //       this.accountNames = data
-  //     } )
-  // }
 }
